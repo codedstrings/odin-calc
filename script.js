294c180d87ee49e52c696e9bfb6a8167b878df07
codedstrings <mridhulml@gmail.com>
2024-02-01 12:17:55 +0530

    add: explanation comments,change: ac button onclick method replaced with addEventlistener in script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 let num1 = 0;
 let num2 = 0;
 let operator;
-let zerroError=false;
+let zerroError = false;
 let displayVal = "";
 let operandCount = 0; //to know when pairs are created
 let largeDisplay = document.querySelector(".large-display");
 let smallDisplay = document.querySelector(".small-display");
-// const element = document.querySelector(".calculator");
 let numberBtns = document.querySelectorAll(".number");
 let operatorBtns = document.querySelectorAll(".operator");
 let equals = document.querySelector(".equals");
+let acBtn=document.querySelector(".ac");
 
 function operate(num1, num2, operator) {
   num1 = Number(num1);
@@ -48,10 +48,8 @@ const subtract = function (num1, num2) {
 };
 
 const divide = function (num1, num2) {
-  if(num2==0){
-    // zeroErrorHandler();
-    // clearFunc();
-    zerroError=true;
+  if (num2 == 0) {
+    zerroError = true;
     return "ERROR";
   }
   return num1 / num2;
@@ -65,19 +63,26 @@ operatorBtns.forEach((operator) => {
   operator.addEventListener("click", operatorClick);
 });
 equals.addEventListener("click", equalsHandler);
+acBtn.addEventListener("click",clearFunc);
 
 function numberClick() {
   console.log("\nNumberclick()");
 
-  if(zerroError){
-    console.log("zerroError")
-    return
+  if (zerroError) {
+    console.log("zerroError");
+    return;
   }
+
+  //displayVal used to concatenate all num entries and make a single number
   displayVal = displayVal + this.value;
+
+  //dipslay current pressed
   largeDisplay.innerText = displayVal;
   if (operandCount == 0) {
+    //first time number is entered
     num1 = displayVal;
   } else if (operandCount == 1) {
+    // number clicked after an operator is clicked. ie. num1 exists
     num2 = displayVal;
     operandCount = 2;
   }
@@ -90,45 +95,51 @@ function operatorClick() {
   console.log("\nOperatorclick()");
 
   if (operandCount == 0) {
+    //for when operator clicked after a number 
+    //click after equals is buggy
     operandCount = 1;
-  } else if (operandCount == 2) {
-    let tempReturn=operate(num1,num2,operator)
-    if(zerroError){
+  } 
+  
+  else if (operandCount == 2) {
+    //when two operands exists
+    let tempReturn = operate(num1, num2, operator);
+
+    //to check for divide by zero errors. 
+    if (zerroError) {
       console.log("zerro error");
-      // smallDisplay.innerText=
-      // largeDisplay.innerText=tempReturn;
+      //equalshandler to print error and show calculation on small display
       equalsHandler();
-      return 
-    }
-    else{
+      return;
+    } 
+    else {
+      //set num1 as result, num2 to zero and operandcount=1 to continue operations.
       num1 = tempReturn;
       num2 = 0;
       operandCount = 1;
     }
-   
   }
 
-
+  //set operator val and print calc on small display and currently pressed on largedisplay
   operator = this.value;
   largeDisplay.innerText = operator;
-  smallDisplay.innerText=num1+" "+operator;
-  displayVal = "";
+  smallDisplay.innerText = num1 + " " + operator;
+  displayVal = ""; //reset displaVal for numberClick to work properly
 
   console.log("displayval:", displayVal, " num1,num2: ", num1, num2);
   console.log("operator,operandcount: ", operator, operandCount);
 }
 
-//A/C
+//onclick A/C button
 function clearFunc() {
+  //resets all values, clears small display and print 0 in largedisplay
   smallDisplay.innerText = "";
   largeDisplay.innerText = 0;
-  zerroError=false;
+  zerroError = false;
   num1 = 0;
   num2 = 0;
   displayVal = "";
   operator = null;
   operandCount = 0;
-  result = 0;
   console.log(
     "cleared: num1,num2,operator,operandcount:",
     num1,
@@ -140,21 +151,14 @@ function clearFunc() {
 
 //equals
 function equalsHandler() {
-  console.log("EqualsHandler")
+  console.log("EqualsHandler");
+  //only print equals when two operands are present. 
   if (operandCount == 2) {
     let calc = operate(num1, num2, operator);
     console.log("equals; ", calc);
     smallDisplay.innerText = num1 + " " + operator + " " + num2 + " = ";
     largeDisplay.innerText = calc;
-    displayVal=calc;
+    //displayVal set to calc so it can be used as num1 again.
+    displayVal = calc;
   }
-}
-function zeroErrorHandler(){
-  numberBtns.forEach((number) => {
-    number.removeEventListener("click", numberClick);
-  });
-  operatorBtns.forEach((operator) => {
-    operator.removeEventListener("click", operatorClick);
-  });
-  equals.removeEventListener("click", equalsHandler);
 }
